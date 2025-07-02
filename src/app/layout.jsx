3c3c26eeb4d7c6850e6059963ec8411af2e70db3
RaviGupta965 <clicktoravi1234@gmail.com>
2025-07-02 17:16:06 +0530

    build-issue-resolved

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,7 @@
 
 import AuthProvider from '@/app/components/AuthProvider';
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from './components/Navbar';
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata = {
   title: 'TrendWise Blog',
